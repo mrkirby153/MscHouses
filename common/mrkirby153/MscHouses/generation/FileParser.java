daaedf4cf45f3de4a28072d6e81ef1318711ffe0
mrkirby153 <mr.austinwhyte@gmail.com>
2013-10-09 18:23:08 -0700

    Fixed (for sure) derpy file stuff
    Git derped and somehow duplicated a file on the remote origin (maybe
    because I had two git clients open?). It apparently failed at merging the
    two files together and I had to go and fix the mistakes manually. Grrr....
    Sometimes I don't know about git

--- common/mrkirby153/MscHouses/generation/FileParser.java
+++ common/mrkirby153/MscHouses/generation/FileParser.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 
 
 public class FileParser {
-<<<<<<< HEAD
+
 
 	public static String getFileContents(String fileLocation){
 		String contents = "";
@@ -15,15 +15,6 @@ public class FileParser {
 		if(file.exists()){
 			try{
 				BufferedReader br = new BufferedReader(new FileReader(fileLocation));
-=======
-	
-	public static String getFileContents(String fileName) throws FileNotFoundException{
-		String contents = "";
-		File file = new File(fileName);
-		if(file.exists()){
-			try{
-				BufferedReader br = new BufferedReader(new FileReader(fileName));
->>>>>>> 97a47056c72c4f70026cfbdaef4d7a64b0ce1311
 				String strLine;
 				while((strLine = br.readLine()) != null){
 					contents += strLine;
@@ -32,7 +23,6 @@ public class FileParser {
 			}catch(Exception e){
 				System.out.println(e.getMessage());
 			}
-<<<<<<< HEAD
 		}
 		return contents;
 	}
@@ -45,21 +35,6 @@ public class FileParser {
 			// Split into x, y, z, id+metadata
 			String[] coordinateParts = coordinateChunk[i].split(":");
 			return_value = new int[coordinateParts.length][coordinateParts.length];
-=======
-			return contents;
-		}else{
-			throw new FileNotFoundException();
-		}
-	}
-	
-	public static int[][][][][] parseFile(String contents){
-		String[] coordinateChunk = contents.split(";");
-		int[][][][][] return_value;
-		for(int i=0; i < coordinateChunk.length; i++){
-			// Split into x, y, z, id+metadata
-			String[] coordinateParts = coordinateChunk[i].split(":");
-			return_value = new int[coordinateParts.length][coordinateParts.length][coordinateParts.length][coordinateParts.length][coordinateParts.length];
->>>>>>> 97a47056c72c4f70026cfbdaef4d7a64b0ce1311
 			String id = coordinateParts[3];
 			String[] blockAndMeta = id.split(",");
 			int x = Integer.parseInt(coordinateParts[0]);
@@ -67,26 +42,17 @@ public class FileParser {
 			int z = Integer.parseInt(coordinateParts[2]);
 			int block_id = Integer.parseInt(id.substring(0, id.length()-2));
 			int block_meta = Integer.parseInt(blockAndMeta[2]);
-<<<<<<< HEAD
 			return_value[i][0] = x;
 			return_value[i][1] = y;
 			return_value[i][2] = z;
 			return_value[i][3] = block_id;
 			return_value[i][4] = block_meta;
-=======
-			return_value[i][0][0][0][0] = x;
-			return_value[0][i][0][0][0] = y;
-			return_value[0][0][i][0][0] = z;
-			return_value[0][0][0][i][0] = block_id;
-			return_value[0][0][0][0][i] = block_meta;
->>>>>>> 97a47056c72c4f70026cfbdaef4d7a64b0ce1311
 			return return_value;
 		}
 		
 		return null;
 	}
 	
-<<<<<<< HEAD
 	public static int[][] parseFile(String fileLocation, int materialId){
 		String contents = getFileContents(fileLocation);
 		String[] coordinateChunk = contents.split(";");
@@ -95,15 +61,6 @@ public class FileParser {
 			// Split into x, y, z, id+metadata
 			String[] coordinateParts = coordinateChunk[i].split(":");
 			return_value = new int[coordinateParts.length][coordinateParts.length];
-=======
-	public static int[][][][][] parseFile(String contents, int materialId){
-		String[] coordinateChunk = contents.split(";");
-		int[][][][][] return_value;
-		for(int i=0; i < coordinateChunk.length; i++){
-			// Split into x, y, z, id+metadata
-			String[] coordinateParts = coordinateChunk[i].split(":");
-			return_value = new int[coordinateParts.length][coordinateParts.length][coordinateParts.length][coordinateParts.length][coordinateParts.length];
->>>>>>> 97a47056c72c4f70026cfbdaef4d7a64b0ce1311
 			String id = coordinateParts[3];
 			String[] blockAndMeta = id.split(",");
 			int x = Integer.parseInt(coordinateParts[0]);
@@ -116,7 +73,6 @@ public class FileParser {
 				block_id = Integer.parseInt(id.substring(0, id.length()-2));
 			}
 			int block_meta = Integer.parseInt(blockAndMeta[2]);
-<<<<<<< HEAD
 			return_value[0][i] = x;
 			return_value[1][i] = y;
 			return_value[2][i] = z;
@@ -137,20 +93,4 @@ public class FileParser {
 		}
 		return false;
 	}
-
-
-=======
-			return_value[i][0][0][0][0] = x;
-			return_value[0][i][0][0][0] = y;
-			return_value[0][0][i][0][0] = z;
-			return_value[0][0][0][i][0] = block_id;
-			return_value[0][0][0][0][i] = block_meta;
-			return return_value;
-		}
-		
-		return null;
-	}
-	
-	
->>>>>>> 97a47056c72c4f70026cfbdaef4d7a64b0ce1311
 }
