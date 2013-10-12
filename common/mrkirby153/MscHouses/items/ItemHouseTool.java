27f0f2621486c7b503179a95541512c0420c9ea0
mrkirby153 <mr.austinwhyte@gmail.com>
2013-10-11 17:13:10 -0700

    Rewrote House Generation code to be more effecent

--- common/mrkirby153/MscHouses/items/ItemHouseTool.java
+++ common/mrkirby153/MscHouses/items/ItemHouseTool.java
@@ -1,18 +1,18 @@
 package mrkirby153.MscHouses.items;
 
-import java.util.ArrayList;
+import java.io.File;
 
-import mrkirby153.MscHouses.api.FuelRegistry;
 import mrkirby153.MscHouses.api.IHouseItem;
 import mrkirby153.MscHouses.api.MaterialRegistry;
 import mrkirby153.MscHouses.configuration.ConfigurationSettings;
 import mrkirby153.MscHouses.core.MscHouses;
+import mrkirby153.MscHouses.lib.ResourceFile;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
@@ -39,50 +39,54 @@ public class ItemHouseTool extends Item {
 	@Override
 	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player) {
 		MovingObjectPosition p = this.getMovingObjectPositionFromPlayer(world, player, true);
-		if(p != null)
-			if(world.getBlockTileEntity(p.blockX, p.blockY, p.blockZ) != null)
-				inventory = (IInventory) world.getBlockTileEntity(p.blockX, p.blockY, p.blockZ);
-		if(inventory != null){
-			boolean fuel = false;
-			if(player.capabilities.isCreativeMode){
-				fuel = true;
-			}
-			int moduelId = 0;
-			int modifyerMaterial = 0;
-			if(inventory.getStackInSlot(0) != null){
-				if(inventory.getStackInSlot(0).getItem() instanceof IHouseItem){
-					((IHouseItem) inventory.getStackInSlot(0).getItem()).geneateInit(world, p.blockX, p.blockY, p.blockZ);
-				}else{
-					if(inventory.getStackInSlot(0).getItem() == MscHouses.moduel)
-						moduelId = inventory.getStackInSlot(0).getItemDamage();
-				}
-			}
-			if(inventory.getStackInSlot(2) != null){
-				modifyerMaterial = MaterialRegistry.materialLookup(inventory.getStackInSlot(2).getItemDamage());
-			}
+		// Run verifications on the block
+		if(p == null)
+			return item;
+		if(!(world.getBlockId(p.blockX, p.blockY, p.blockZ) == MscHouses.BlockHouseGenerator.blockID))
+			return item;
+		if(world.getBlockTileEntity(p.blockX, p.blockY, p.blockZ) == null)
+			return item;
+		inventory = (IInventory) world.getBlockTileEntity(p.blockX, p.blockY, p.blockZ);
+		if(inventory == null)
+			return item;
+		// End verifications
+		boolean fuel = false;
 
-			if(inventory.getStackInSlot(1) != null){
-				ArrayList<Item> valid = FuelRegistry.getValidFuelsAsArrayList();
-				if(valid.contains(inventory.getStackInSlot(1).getItem())){
-					fuel = true;
-				}else{
-					if(!world.isRemote){
-						player.sendChatToPlayer(ChatMessageComponent.createFromText("No valid fuel detected in inventory"));
-					}
-				}
-			}
-			if(fuel){
-				if(!player.capabilities.isCreativeMode){
-					inventory.setInventorySlotContents(0, null);
-					inventory.setInventorySlotContents(1, null);
-					inventory.setInventorySlotContents(2, null);
-					player.addPotionEffect(new PotionEffect(15, 200, 0, true));
-					player.addPotionEffect(new PotionEffect(9, 200, 0, true));
-				}
-				buildHouse(moduelId, modifyerMaterial, p.blockX, p.blockY, p.blockZ, world);
+		if(player.capabilities.isCreativeMode)
+			fuel = true;
 
-			}
+		String moduelFile = "";
+		int modifyerMaterial = 0;
+		// Run verification on items
+		if(inventory.getStackInSlot(0) == null){
+			player.sendChatToPlayer(ChatMessageComponent.createFromText("You are missing a module"));
+			return item;
+		}
 
+		if(inventory.getStackInSlot(1) == null){
+			player.sendChatToPlayer(ChatMessageComponent.createFromText("You are missing fuel"));
+			return item;
+		}
+		if(inventory.getStackInSlot(2) == null){
+			player.sendChatToPlayer(ChatMessageComponent.createFromText("You are missing a material modifier"));
+		}
+		// End verification
+
+		// Get modifier material
+		modifyerMaterial = MaterialRegistry.materialLookup(inventory.getStackInSlot(2).getItemDamage());
+		
+		// Run verifications on the module
+		if(inventory.getStackInSlot(0).getItem() instanceof IHouseItem){
+			((IHouseItem)inventory.getStackInSlot(0).getItem()).geneateInit(world, p.blockX, p.blockY, p.blockZ);
+		}else{
+			if(inventory.getStackInSlot(0).getItem() == MscHouses.moduel){
+				ItemModuel module = (ItemModuel) inventory.getStackInSlot(0).getItem();
+				String fileName = module.moduel_name[inventory.getStackInSlot(0).getItemDamage()];
+				String filePath = ResourceFile.houseGen_loc_base + fileName+".house";
+				System.out.println("Expected generation file at: "+filePath);
+				File file = new File(filePath);
+				if(!file.exists()) System.out.println("File does not exist!");
+			}
 		}
 
 		return item;
