9e7defafd2db835d49881adace4b77ea7793bd52
mrkirby153 <mr.austinwhyte@gmail.com>
2013-10-13 11:01:04 -0700

    Createing generation command

--- common/mrkirby153/MscHouses/core/command/MscHousesCommand.java
+++ common/mrkirby153/MscHouses/core/command/MscHousesCommand.java
@@ -1,5 +1,6 @@
 package mrkirby153.MscHouses.core.command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import mrkirby153.MscHouses.core.MscHouses;
@@ -32,10 +33,12 @@ public class MscHousesCommand extends CommandBase {
 		return true;
 	}
 
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public List getCommandAliases() {
-		return null;
+		List aliasses = new ArrayList<>();
+		aliasses.add("mschouses");
+		return aliasses;
 	}
 
 	@Override
@@ -51,10 +54,76 @@ public class MscHousesCommand extends CommandBase {
 			sender.sendChatToPlayer(ChatMessageComponent.createFromText("Format: '" + this.getCommandName() + " <command> <arguments>'"));
 			sender.sendChatToPlayer(ChatMessageComponent.createFromText("Available commands:"));
 			sender.sendChatToPlayer(ChatMessageComponent.createFromText("- version : Version information."));
+			sender.sendChatToPlayer(ChatMessageComponent.createFromText("- create: Create a new house"));
 			return;
+		} else if(arguments[0].matches("create")){
+			if(arguments.length < 10){
+				//x, y, z, widht, height, legnth, name, floorDepth, specal, specalid
+				if(arguments.length == 2){
+					if(arguments[1].matches("help")){
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("Format: '"+this.getCommandName()+ " create <x> <y> <z> <name> <floorDepth>"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("Arguments:"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("- x: The starting x coordinate for the structure"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("- y: The starting x coordinate for the structure"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("- z: The starting x coordinate for the structure"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("- name: The name of the structure."));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("- floorDepth: How deep the floor goes"));
+						sender.sendChatToPlayer(ChatMessageComponent.createFromText("Example Usage: /"+this.getCommandName()+" create 124 40 -34 foo 3"));
+						return;
+					}else{
+						throw new WrongUsageException("Type '"+this.getCommandName()+ " create help' for help.");
+					}
+				}else if(arguments.length ==1){
+					throw new WrongUsageException("Type '"+this.getCommandName()+" create help' for help");
+				}
+			}else{
+				// sender.sendChatToPlayer(ChatMessageComponent.createFromText("[DEBUG] X: "+arguments[1]+" Y: "+arguments[2]+" Z: "+arguments[3]+" Name: "+arguments[4] + " FloorDepth: "+arguments[5]));
+				// Begin parsing file
+				if(!isNumeric(arguments[1])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eX Coordinate is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[2])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eY Coordinate is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[3])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eZ Coordinate is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[4])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eFloor Depth is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[5])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eLegnth is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[6])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eHeight is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[7])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eWidth is not a number!"));
+					return;
+				}
+				if(!isNumeric(arguments[10])){
+					sender.sendChatToPlayer(ChatMessageComponent.createFromText(MscHouses.COLOR_CODE+"eSpecal ID is not a number!"));
+					return;
+				}
+				int x = Integer.parseInt(arguments[1]);
+				int y = Integer.parseInt(arguments[2]);
+				int z = Integer.parseInt(arguments[3]);
+				int floorDepth = Integer.parseInt(arguments[4]);
+				int width = Integer.parseInt(arguments[5]);
+				int height = Integer.parseInt(arguments[6]);
+				int legnth = Integer.parseInt(arguments[7]);
+				String name = arguments[8];
+				boolean specal = (arguments[9].matches("true"))? true : false;
+				int specalId = Integer.parseInt(arguments[10]);
+				System.out.println(x+":"+y+":"+z+":"+floorDepth+":"+width+":"+height+":"+legnth+":"+name+":"+specal+":"+specalId);
+			}
 		}
-		
-		throw new WrongUsageException(this.getCommandUsage(sender));
 	}
 
 	private void commandVersion(ICommandSender sender, String[] arguments) {
@@ -67,6 +136,15 @@ public class MscHousesCommand extends CommandBase {
 				sender.sendChatToPlayer(ChatMessageComponent.createFromText("\u00A79" + updateLine));
 			}
 		}
+	}
+	
+	private static boolean isNumeric(String str){
+		try{
+			double d = Double.parseDouble(str);
+		}catch(NumberFormatException e){
+			return false;
+		}
+		return true;
 	}
 
 }
